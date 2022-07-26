f2ce8571e5d9cb7ee21ce4b8b0f23e1957e464fe
soutsos <106550353+Sidiro23@users.noreply.github.com>
2022-07-25 20:42:22 -0500

    installed express/started server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+
+//requires express module
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+// instantiate the server
+const app =express();
+
+//port 
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended : true}));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/api/notes', (req, res)=>{
+  fs.readFile('./db/db.json', 'utf8', (err, data)=>{
+    if (err) throw err;
+    let notes= JSON.parse(data);
+    let userNote = req.body;
+    userNote.id = Math.floor(math.random()* 5000);
+    notes.push(userNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err,data)=>{
+      res.json(userNote);
+    });
+  });
+});
